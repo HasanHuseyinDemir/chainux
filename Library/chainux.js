f39d07cedc8ad348769663da269d9344c6d1d385
HasanHuseyinDemir <majorexo565@gmail.com>
2024-10-15 02:43:55 +0300

    CHNX:Reactive indevelopment hooks,datacomponent,clean,improvements

--- Library/chainux.js
+++ Library/chainux.js
@@ -1,202 +1,139 @@
-export const {html,components,onConnect,onRemove,Render,Data,IStore}=(()=>{
+export const {html,components,onConnect,onRemove,Render}=(()=>{
 const key="#!CHNX!#"
-const components = componentMixin();
-/*Interact*/
-//lifecycle hooks
-//html.plain
-HTMLElement.prototype.clear=function(){this.textContent="";return this}
-HTMLElement.prototype.hide=function(a){this.style.display=a?"none":this.style.display;return this};
-HTMLElement.prototype.show=function(a){this.style.display=a?"block":this.style.display;return this};
-Object.defineProperty(HTMLElement.prototype,"text",{
-    set:function(v){this.textContent=v},
-    get:function(){return this.textContent},
-});
-HTMLElement.prototype.addClass=function(c){this.classList.add(c);return this}
-HTMLElement.prototype.removeClass=function(c){this.classList.remove(c);return this}
-HTMLElement.prototype.toggleClass=function(c){this.classList.toggle(c);return this}
-HTMLElement.prototype.setStyle = function(styles) {
-    for (let key in styles) {
-        let value=styles[key];
-        if (typeof value=="function") {
-            value=value.call(this);
-        }
-        if (this.style[key]!=value) {
-            this.style[key]=value;
-        }
-    }
-    return this;
-};
-HTMLElement.prototype.set = function(p,arg){
-    if(p.style){
-        if(typeof p.style=="object"&&typeof p.style!="function"){
-            this.setStyle(p.style)
-        }else{
-            this.style=typeof p.style=="function"?p.style.call(this):p.style
-        }
-    }
-    for (let key in p) {
-        if(key!="callback"&&key!="style"){
-            if (key.startsWith("on")){
-                this[key]=p[key].bind(this)
-                continue
+const components = (()=>{
+    const map = new Map();
+    return {
+        get(key,accessKEY) {
+            if(accessKEY==$ACCESS){
+                if(map.has(key)){
+                    return map.get(key);
+                }else{
+                    console.warn("Component<"+key+"> is not defined!")
+                }
+            }else{
+                console.error("Access Denied!")
             }
-            let value=p[key];
-            if(typeof value=="function") {
-                let result=value.call(this,arg);
-                if (this[key]!==result){
-                    this[key]=result;
-                }
-            } 
-            else if(key in this) {
-                if (this[key]!==value) {
-                    this[key]=value;
-                }
-            } 
-            else{
-                if(this.getAttribute(key)!==value) {
-                    this.setAttribute(key,value);
-                }
-            }
-        }
-    }
-    if(p.callback){
-        if(typeof p.callback=="function"){
-            p.callback.call(this)
-        }else{
-            console.warn("Set Callback Must Be a Function!")
-        }
-    }
-    return this
-};
-
-let index=0
-function IStore(obj,ca){//Interactive Store || NON REACTIVE
-    let c=ca//callback
-    let id=index++
-    if(!observer){
-        defineMObserver()
-    }
-    return new Proxy(obj,{
-        get(t,k){
-            return t[k]
         },
-        set(t,k,v){
-            let before=t[k]
-            if(t[k]!=v){
-                t[k]=v
-                let call={before,current:v,key:k,id}
-                hooks.mountedISElements.forEach((val,key)=>{key.set(val,call)})//Update!
-                if(c){
-                    c.call(call)
-                }
+        set(key, value) {
+            if (typeof key !== 'string') {
+                console.error("Key must be a string.");
+                return;
             }
-            return 1
+            if (typeof value !== 'function') {
+                console.error("Value must be a component-function.");
+                return;
+            }
+
+            if(key.includes(" ")){
+                console.error("Component names should not contain spaces.");
+                return 
+            }
+
+            if(map.has(key.toLowerCase())){
+                console.warn("Component <"+key+"> already set!")
+            }
+            map.set(key.toLowerCase(), value);
+        },
+    };
+})()
+
+//SYMBOLS
+let $CLEANUP=Symbol()
+let $APPEND=Symbol()
+let $ACCESS=Symbol()
+let $SETHOOKS=Symbol()
+let $MEMO=Symbol()
+
+//COMPLETE
+new MutationObserver((M)=>{
+    M.forEach((m)=>{
+        if (m.type=="childList"){
+            m.removedNodes.forEach((r)=>{collect(r).forEach(e=>{if(e[$CLEANUP]){e[$CLEANUP]()}})})
+            m.addedNodes.forEach((a)=>{collect(a).forEach(e=>{if(e[$APPEND]){e[$APPEND]()}})})
         }
-    })
-}
-
-const hooks={
-    temp:{},
-    elementHooksPair:new WeakMap(),
-    elementDataPair:new WeakMap(),
-    elementISPair:new WeakMap(),//interactive state pair
-    mountedISElements:new Map(),//Ekle sil işlemleri mount unmount ile yapılır!
-    eMSG:"Component onConnect & onRemove hooks must be a function!"
-}
-
-//Element için onSee gibi özellikler eklenebilir
-//WIP + onEvent + Dispatch(document.bodyden bütün sayfaya veya istenen noktaya)
-function Data(d) {
-    return hooks.temp.data=d
-}
-Data.hasData=function(e){
-    return hooks.elementDataPair.has(e)
-}
-Data.getData=function(e){
-    if(Data.hasData(e)){return hooks.elementDataPair.get(e)}
-}
-/*WIP Lazy Component Loading is best for optimizations
-function lazyComponent(e){
-    hooks.lazyComponent=e??true
-}
-*/
-
-let observer
-function defineMObserver(){
-    observer = new MutationObserver((mutationsList) => {
-        mutationsList.forEach(mutation=>{
-            if (mutation.type == "childList") {
-                mutation.addedNodes.forEach((node) => {
-                    if (node instanceof HTMLElement) {                        
-                        [node,...node.querySelectorAll("*")].forEach(e=>{
-                            if(hooks.elementISPair.has(e)){
-                                let data=hooks.elementISPair.get(e)
-                                hooks.mountedISElements.set(e,data)
-                                e.set(data)
-                            }
-                            callHook("connect",e)
-                        })
-                    }
-                });
-                mutation.removedNodes.forEach((node) => {
-                    if (node instanceof HTMLElement) {
-                        [node,...node.querySelectorAll("*")].forEach(e=>{
-                            if(hooks.mountedISElements.has(e)){
-                                hooks.mountedISElements.delete(e)
-                            }
-                            callHook("remove",e)
-                        })
-                    }
-                });
-            }
-        });
     });
-    observer.observe(document.body, { childList: true, subtree: true });
+}).observe(document.body,{childList:true,subtree:true});
+
+//TYPES
+//COMPLETE
+let isImmut=c=>typeof c=="string"||typeof c=="number"
+let isObj=c=>c?Object.getPrototypeOf(c)==Object.prototype:!1
+
+//COMPLETE
+function destroy(o){
+    if(o instanceof Node){
+        let c=collect(o)
+        c.forEach(e=>e.remove())
+        c.forEach(e=>e[$CLEANUP]?e[$CLEANUP]():"")
+        c=null
+        return
+    }
+    for(let val in o){
+        let t=o[val]
+        if(t&&isObj(t)){destroy(t)}
+        else if(t instanceof Map){t.forEach((v,k)=>{destroy(v);t.delete(k);})}
+        else if(t instanceof Set){t.forEach(v=>{destroy(v)});t.clear()}
+        else if(Array.isArray(t)){t.forEach(v=>{destroy(v)})}
+        delete o[val]
+    }
+}
+//COMPLETE
+function collect(e){
+    const ns = [];
+    function t(n) {
+        if (n.nodeType==1||n.nodeType==3){ns.push(n)}
+        if(n.childNodes){
+            n.childNodes.forEach(c=>t(c));
+        }
+    }
+    t(e);
+    try{
+        return [...new Set(ns)];
+    }finally{
+        t=null
+        ns=null
+    }
 }
 
-function defineHook(n,f){if(typeof f=="function"){hooks.temp[n]=f}else{console.warn(hooks.eMSG)};/*for optimization*/if(!observer){defineMObserver()}}
-
-function callHook(m,e){//method element
-    let target=hooks.elementHooksPair
-    if(target.has(e)&&target.get(e)[m]){target.get(e)[m].call(e,e)}
+const global={
+    renders:[],
+    stateContext:[]
 }
-
-let onConnect=f=>{defineHook("connect",f)}
-let onRemove=f=>{defineHook("remove",f)}
-let c=()=>{hooks.temp.connect=null;hooks.temp.remove=null;hooks.temp.data=null}//ClearHooks
 
 //for Components
 function Render(f,call){
-    c()
+    let g=global.renders
+    g.push[{hooks:{},watchers:[]}]
     let o=f.call(call)//output
-    let {remove,connect,data}=hooks.temp
-    if(connect||remove){
-        hooks.elementHooksPair.set(o,{remove,connect})
-    }
-    if(data){
-        hooks.elementDataPair.set(o,data)
-    }
-    c()
+    o.forEach(e=>{
+        if(e[$SETHOOKS]){
+            e[$SETHOOKS]()
+        }
+    })
+
     return o
 }
 
 
-/**/
+//COMPLETE
 function HTML(string) {return document.createRange().createContextualFragment(string)}
 
+//COMPLETE
 function processElement(element) {
-    const walker = document.createTreeWalker(element, NodeFilter.SHOW_TEXT, null, false);
-    let node;
-
-    while ((node = walker.nextNode())) {
-        if (node.textContent.includes(key)) {
-            replaceKeyWithElement(node);
+    const W = document.createTreeWalker(element, NodeFilter.SHOW_TEXT, null, false);
+    let n;
+    while ((n = W.nextNode())) {
+        if (n.textContent.includes(key)) {
+            replaceKeyWithElement(n);
         }
     }
+    W=null
+    node=null
 }
 
 //<Component class="x" id="y"/> => <Component class="x" id="y"></Component>
 //https://gist.github.com/HasanHuseyinDemir/ba781df027f4271ff9aa9b2e978a0afd
+//COMPLETE
 function nM(e) {
     let s = e,
         n = s.split("/>").length - 1,
@@ -217,6 +154,7 @@ function nM(e) {
     return s;
 }
 
+//COMPLETE
 function replaceKeyWithElement(textNode) {
     const range = document.createRange();
     let text = textNode.textContent;
@@ -232,38 +170,12 @@ function replaceKeyWithElement(textNode) {
         text = textNode.textContent;
         index = startIndex + "<chnx></chnx>".length;
     }
+    range=null
+    text=null
+    index=null
 }
 
-function componentMixin() {
-    const map = new Map();
-    return {
-        get(key) {
-            if(map.has(key)){
-                return map.get(key);
-            }else{
-                console.warn("Component<"+key+"> is not defined!")
-            }
-        },
-        set(key, value) {
-            if (typeof key !== 'string') {
-                console.error("Key must be a string.");
-                return;
-            }
-            if (typeof value !== 'function') {
-                console.error("Value must be a component-function.");
-                return;
-            }
-
-            if(key.includes(" ")){
-                console.error("Component names should not contain spaces.");
-                return 
-            }
-            map.set(key.toLowerCase(), value);
-        },
-        map
-    };
-}
-
+//COMPLETE
 function collectSlots(e) {
     let slots = {};
     let frag=new DocumentFragment()
@@ -279,14 +191,14 @@ function collectSlots(e) {
 return { slot: frag, slots };
 }
 
-
+//COMPLETE but must be a ASYNC
 function componentProcess(target){
     let temp=document.createElement("div")
     //slotları documentFragmente aktarmak
     target.replaceWith(temp)
     //sadece this.props,this.slot dışarıdan alınacaktır call edilerek çalıştırılacaktır
     //tekil elementleri render etmek için kullanılır
-    let compiled=Render(components.get(target.tagName.toLowerCase()),this)//isteğe bağlı slot ve proplar işlenebilir
+    let compiled=Render(components.get(target.tagName.toLowerCase(),$ACCESS),this)//isteğe bağlı slot ve proplar işlenebilir
     if(compiled instanceof HTMLElement){
         //doğru yol
         //slot parçalama işi burada yapılacaktır
@@ -310,23 +222,22 @@ function componentProcess(target){
 
 function html(e,...ar){
     let str
-    let data=hooks.temp.data
     let args=[...ar]
     e.forEach((a,i)=>{str+=a;args.length!==i?str+=key:""})
-    str=str.replaceAll("<>", "<div>").replaceAll("</>", "</div>");
+    let fragmented=str.includes("<>")
+    str=str.replaceAll("<>", "<div>").replaceAll("</>", "</div>");//TEMPORARY
     if(str.includes("/>")){str=nM(str)}
-    let fragment = HTML(str)
-    let element=fragment.firstElementChild
+    let element=HTML(str).firstElementChild
     let subcomponents=[]
     let fC=false//is first element component?
     let qsA=()=>[element,...element.querySelectorAll("*")]
-        qsA().forEach((elm,i)=>{
+        qsA().forEach((elm)=>{
             if (elm.textContent.includes(key)) {
                 processElement(elm)
             }
         })
         function process(elm,i){
-            if(components.map.has(elm.tagName.toLowerCase())){
+            if(components.get(elm.tagName.toLowerCase(),$ACCESS)){
                 let values={}
                 if(i==0){fC=true}
                 if(elm.hasAttributes()){
@@ -425,18 +336,70 @@ function html(e,...ar){
             }
         }
         qsA().forEach(process)
+
+
+        //BURAYA ÜST DATA ELEMENTLERİNDEN TOPLANILAN VERİLERDE OLMALI
+
     //}
     subcomponents.forEach(x => {
-        let c = componentProcess.call({props: x.values,...collectSlots(x.target),parent:element,data},x.target);
+        let c = componentProcess.call({props: x.values,...collectSlots(x.target),parent:element},x.target);
         if (fC) {element=c}
     });
-    return element
+
+    //TEMPORARY CLEANUP
+    str=null
+    args=null
+
+
+
+    if(!fragmented){
+        return element
+    }else{
+        let frag=new DocumentFragment()
+        frag.append(...element.childNodes)
+        element.remove()
+        return frag
+    }
 }
 
-html.plain=function(e){
-    return HTML(e)
-}
-HTMLElement.prototype.inner=function(e){this.clear();this.appendChild(e)}
+let hooksGen=new Proxy({},{
+    get(_,a){return function(f){
+        let t=global.renders
+        let last=t[t.length-1]
+        if(t.length){
+            if(!last.hooks[a])last.hooks[a]=[]
+            last.hooks[a].push(f)
+        }else{
+            console.error(`Chainux HookSet-${a.toUpperCase()} Warn:\nThis hook must be in a component scope!`)
+        }
+        f=null
+        t=null
+        last=null
+    }}
+})
+const {onConnect,onRemove,onEffect,onSignal}=hooksGen
+hooksGen=null
 
-return {html,components,onConnect,onRemove,Render,Data,IStore}
+
+html.plain=HTML
+
+
+//HTMLElement.prototype.inner=function(e){this.clear();this.appendChild(e)}
+
+function Data(props){
+    let e=html`<div><slot/></div>`
+    if(props.name==undefined&&typeof props.name!="string"){
+        console.error("Data Component requires a 'name' property of type string to function properly.")
+        return e
+    }
+    e[$MEMO]=true
+
+    let data={[props.name]:props.data}
+    e[$ACCESS]=()=>data
+    return e
+}
+components.set("data",Data)
+
+
+return {html,components,onConnect,onRemove,Render}
 })()
